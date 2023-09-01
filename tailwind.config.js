454a14dbb3048f1841f0e12fad536f8630e5bc0a
abukawsar45 <kawsar1146236@gmail.com>
2023-09-01 11:23:02 +0600

    setup tailwind material

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const withMT = require('@material-tailwind/react/utils/withMT');
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+module.exports = withMT ({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // 
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  
   ],
   theme: {
     extend: {
@@ -15,4 +22,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
